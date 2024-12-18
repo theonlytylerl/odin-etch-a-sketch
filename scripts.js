f635b3b9a551be3e70215dfccf92c662a7232140
theonlytylerl <107710735+theonlytylerl@users.noreply.github.com>
2024-12-18 14:15:31 -0600

    Add ability to resize the grid by user input!

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
-grid = (height, width) => {
-    let container = document.querySelector(".container")
-    for (let i = 0; i < height * width; i++) {
+makeGrid = (userHeight, userWidth) => {
+    let container = document.querySelector(".container");
+    for (let i = 0; i < userHeight * userWidth; i++) {
         const etchBox = document.createElement("div");
         etchBox.classList.add("boxes");
         etchBox.addEventListener("mouseover", () => {
@@ -8,12 +8,17 @@ grid = (height, width) => {
         });
         container.appendChild(etchBox);
     }
+} 
+
+grid = (height, width) => {
+    makeGrid(height, width);
 }
+
 
 const resizeButton = document.querySelector(".resizeButton");
 resizeButton.addEventListener("click", () => {
-    let userHeight = prompt("Enter the new height you want (up to 100).");
-    let userWidth = prompt("Enter the width you want to go with that (up to 100)");
+    userHeight = prompt("Enter the new height you want (up to 100).");
+    userWidth = prompt("Enter the width you want to go with that (up to 100)");
     while (userHeight > 100 || userHeight > 100 || userHeight < 1 || userWidth < 1) {
         if (userHeight > 100 || userHeight < 1) {
             userHeight = prompt("Height Error, enter a new value");
@@ -23,6 +28,19 @@ resizeButton.addEventListener("click", () => {
             userWidth = prompt("Width Error, enter a new width");
         }
     }
+
+    const container = document.querySelector(".container");
+    container.style.width = 15.81 * userWidth + "px";
+    container.style.height = 15.81 * userHeight + "px";
+
+    let numberOfBoxes = container.childElementCount;
+    for (let i = 0; i < numberOfBoxes; i++) {
+        const box = document.querySelector(".container>div");
+        console.log(box);
+        container.removeChild(box);
+    }
+
+    makeGrid(userHeight, userWidth);
 });
 
 grid(16, 16)
